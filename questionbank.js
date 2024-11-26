a6e228cb04406a7e8af4a6d0809cc8fc47d55047
satwikteotia <satwikteotia45@gmail.com>
2024-11-26 12:09:53 +0530

    Add files via upload

---
+++ questionbank.js
@@ -0,0 +1,24 @@
+const questions = [
+	{
+		question: 'What is React',
+		options: ['Library', 'Software', 'App', 'Framework'],
+		answer: 'Library',
+		id:1
+	}, 
+	{
+	question: 'Html stand for...',
+	options: ['Hyper test Markup', 'Hyper', 
+			'Hyper text Markup Language', 'All of the above'],
+	answer: 'Hyper text Markup Language',
+	id:2
+	},
+
+	{
+		question: 'CSS used for..',
+		options: ['Functioning', 'Styleling', 'coding', 'None'],
+		answer: 'Styleling',
+		id:3
+	},
+
+];
+export default questions;
